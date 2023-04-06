14078fd02b099884827ecfa62fbc59581bd4b8f7
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2023-04-06 18:41:12 -0400

    Remove flex-row-reverse from login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 const Login = () => {
   return (
     <header className="hero flex flex-col items-center justify-center bg-base-100">
-      <div className="hero-content flex-col lg:flex-row-reverse lg:gap-x-11">
+      <div className="hero-content flex-col lg:flex-row lg:gap-x-24">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Welcome to MDOC Assistant!</h1>
           <p className="py-6">
