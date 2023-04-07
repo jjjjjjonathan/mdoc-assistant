b1d412b6766e7c846d0b59da4c53c5a9faf549db
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2023-04-06 21:46:03 -0400

    Add header and match list to welcome component

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,22 +1,60 @@
 import { api } from "~/utils/api";
+import { useUser } from "@clerk/nextjs";
+
+const UpcomingMatch = ({ homeTeam }: { homeTeam: string }) => {
+  return (
+    <>
+      <div className="divider" />
+      <p>{homeTeam}</p>
+    </>
+  );
+};
 
 const Welcome = () => {
-
   const { data, isLoading } = api.matches.getUpcomingUserMatches.useQuery();
+  const { user, isSignedIn } = useUser();
 
   if (isLoading) return <p>LOADING</p>;
   if (!data) return <p>something went wrong</p>;
 
   return (
     <>
-      <p>I should have pulled two matches in data. I have {data.length} and they are:</p>
-      <ul>
-        {data.map((match) => (
-          <li key={match.id}>{match.homeTeam.name}</li>
-        ))}
-      </ul>
+      <div className="hero flex flex-col items-center justify-center bg-base-100">
+        <div className="hero-content flex-col lg:flex-row lg:gap-x-24">
+          <header className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">
+              Hi{isSignedIn && <span>, {user.firstName}</span>}!
+            </h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </header>
+          <div className="card w-full max-w-sm flex-shrink-0 bg-neutral shadow-2xl">
+            <div className="card-body">
+              <h2 className="card-title">
+                {data.length === 0 && "You have no upcoming matches."}
+                {data.length > 0 && "Here are your upcoming matches."}
+              </h2>
+              {data.map((match) => (
+                <UpcomingMatch homeTeam={match.homeTeam.name} key={match.id} />
+              ))}
+              <div className="divider" />
+              <div className="card-actions flex-nowrap">
+                <button className="btn-primary btn w-1/2 shrink text-primary-content">
+                  View your matches
+                </button>
+                <button className="btn-secondary btn w-1/2 shrink text-secondary-content">
+                  Create a new match
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Welcome;
