147f36a3cb6db15953c7343a502443a3215bab14
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2023-04-14 10:54:47 -0400

    Temporarily disable es-lint rules for serverPath line

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -14,6 +14,7 @@ export const isReadableWhiteFont = (hex: ColorInput) => {
 
 export const serverPath = (staticFilePath: string) => {
   return path.join(
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
     getConfig().serverRuntimeConfig.PROJECT_ROOT,
     staticFilePath
   );
